e89a8d3c1c88cebf3c68b442df617a8b90e5271f
GR <guiguiribeiro.2020@gmail.com>
2022-03-16 23:16:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// Configuração para rota Express
-const express = require('express');
-const app = express();
-app.get('/', (request, response) => {
-  const ping = new Date();
-  ping.setHours(ping.getHours() - 3);
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-
 require('dotenv').config()
 
 // Utilização da Aoi.JS
